90778bf13f11bb2829da8aaaf9a40500f13688b7
Alex0007 <Alex0007lolpvp@gmail.com>
2020-03-26 01:06:45 +0500

    added filters

--- src/handlers.ts
+++ src/handlers.ts
@@ -1,6 +1,6 @@
 import { WorkerHandlers } from "@watchedcom/sdk";
 import { parseList, parseItem } from "./ted-scraper";
-import * as qs from "querystring";
+import * as qs from "qs";
 
 export const directoryHandler: WorkerHandlers["directory"] = async (
     input,
@@ -10,18 +10,62 @@ export const directoryHandler: WorkerHandlers["directory"] = async (
     const { fetch } = ctx;
     const cursor: number = <number>input.cursor || 1;
 
+    const queryString = qs.stringify(
+        {
+            page: cursor,
+            q: input.search || undefined,
+            ...input.filter,
+        },
+        { arrayFormat: "brackets" }
+    );
+
     const results = await fetch(
-        "https://www.ted.com/talks?" +
-            qs.stringify({
-                page: cursor,
-                q: input.search || undefined,
-            })
+        "https://www.ted.com/talks?" + queryString
     ).then(async (resp) => {
         return parseList(await resp.text());
     });
 
     return {
         nextCursor: results.length ? cursor + 1 : null,
+        features: {
+            filter: [
+                {
+                    id: "language",
+                    name: "Language",
+                    values: [
+                        { value: "English", key: "en" },
+                        { value: "Español", key: "es" },
+                        { value: "日本語", key: "ja" },
+                        { value: "Português brasileiro", key: "pt-br" },
+                        { value: "中文 (繁體)", key: "zh-tw" },
+                        { value: "한국어", key: "ko" },
+                    ],
+                },
+                {
+                    id: "duration",
+                    name: "Duration",
+                    values: [
+                        { value: "0–6 minutes", key: "0-6" },
+                        { value: "6–12 minutes", key: "6-12" },
+                        { value: "12–18 minutes", key: "12-18" },
+                        { value: "18+ minutes", key: "18+" },
+                    ],
+                },
+                {
+                    id: "topics",
+                    name: "Topics",
+                    multiselect: true,
+                    values: [
+                        { value: "Technology", key: "Technology" },
+                        { value: "Entertainment", key: "Entertainment" },
+                        { value: "Design", key: "Design" },
+                        { value: "Business", key: "Business" },
+                        { value: "Science", key: "Science" },
+                        { value: "Global issues", key: "Global issues" },
+                    ],
+                },
+            ],
+        },
         items: results.map((item) => {
             const id = item.link;
             return {
