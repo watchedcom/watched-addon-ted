58eb9e1890459fc37d980f01a236c1814af78cc4
Alex0007 <Alex0007lolpvp@gmail.com>
2020-03-25 18:43:42 +0500

    add sample from website

---
+++ __INITIAL_DATA__.js
@@ -0,0 +1,259 @@
+/**
+ * Sample data that is stored inside HTML from the page:
+ * https://www.ted.com/talks/alanna_shaikh_why_covid_19_is_hitting_us_now_and_how_to_prepare_for_the_next_outbreak/up-next
+ */
+module.exports = {
+    comments: null,
+    threadId: null,
+    requested_language_english_name: "English",
+    current_talk: "61316",
+    description:
+        "In January 1953, a tidal surge shook the North Sea. The titanic waves flooded the Dutch coastline, killing almost 2,000 people. 54 years later, a similar storm threatened the region. But this time, they were ready. This was thanks to a massive, interlocking system known as the Delta Works— the most sophisticated flood prevention project in the world. Stefan Al dives into this marvel of engineering. [Directed by JodyPrody, narrated by Addison Anderson, music by André Aires / AIM Creative Studios].",
+    event: "TED-Ed",
+    language: "en",
+    language_swap: false,
+    name: "Stefan Al: Why isn't the Netherlands underwater?",
+    slug: "stefan_al_why_isn_t_the_netherlands_underwater",
+    series_slug: null,
+    speakers: [
+        {
+            id: "4061",
+            slug: "stefan_al",
+            is_published: false,
+            firstname: "Stefan",
+            lastname: "Al",
+            middleinitial: "",
+            title: "",
+            description: "Architect",
+            photo_url:
+                "https://pe.tedcdn.com/images/ted/7f30930b0514fe977ef29739f17a76ad8da29d73_254x191.jpg",
+            whatotherssay: "",
+            whotheyare:
+                "Architect and urban designer Stefan Al believes that architecture is more than just buildings.",
+            whylisten: "",
+        },
+    ],
+    url:
+        "https://www.ted.com/talks/stefan_al_why_isn_t_the_netherlands_underwater",
+    viewed_count: 0,
+    talks: [
+        {
+            explore_cta_experiment: {},
+            curator_approved: true,
+            description:
+                "In January 1953, a tidal surge shook the North Sea. The titanic waves flooded the Dutch coastline, killing almost 2,000 people. 54 years later, a similar storm threatened the region. But this time, they were ready. This was thanks to a massive, interlocking system known as the Delta Works— the most sophisticated flood prevention project in the world. Stefan Al dives into this marvel of engineering. [Directed by JodyPrody, narrated by Addison Anderson, music by André Aires / AIM Creative Studios].",
+            downloads: {
+                id: 61316,
+                languages: [
+                    {
+                        languageName: "English",
+                        endonym: "English",
+                        languageCode: "en",
+                        ianaCode: "en",
+                        isRtl: false,
+                    },
+                ],
+                nativeDownloads: {
+                    low:
+                        "https://download.ted.com/talks/StefanAl_DeltaWorks_2019E-light.mp4?apikey=acme-roadrunner",
+                    medium:
+                        "https://download.ted.com/talks/StefanAl_DeltaWorks_2019E.mp4?apikey=acme-roadrunner",
+                    high:
+                        "https://download.ted.com/talks/StefanAl_DeltaWorks_2019E-480p.mp4?apikey=acme-roadrunner",
+                },
+                subtitledDownloads: null,
+                audioDownload: null,
+            },
+            duration: 296,
+            event: "TED-Ed",
+            institute_partner_name: null,
+            salon_partner_name: null,
+            event_badge: null,
+            is_featured: false,
+            hero:
+                "https://s3.amazonaws.com/talkstar-photos/uploads/2ea833fe-d1ba-412b-9b77-a8ab1dfda151/deltaworkstextless.jpg",
+            hero_load:
+                "https://pi.tedcdn.com/r/talkstar-photos.s3.amazonaws.com/uploads/2ea833fe-d1ba-412b-9b77-a8ab1dfda151/deltaworkstextless.jpg?q=50&w=15",
+            id: "61316",
+            player_talks: [
+                {
+                    id: "61316",
+                    mediaIdentifier: "StefanAl_DeltaWorks_2019E",
+                    duration: 296,
+                    languages: [
+                        {
+                            languageName: "English",
+                            endonym: "English",
+                            languageCode: "en",
+                            ianaCode: "en",
+                            isRtl: false,
+                        },
+                    ],
+                    nativeLanguage: "en",
+                    isSubtitleRequired: false,
+                    introDuration: 0,
+                    adDuration: 0,
+                    postAdDuration: 0,
+                    resources: {
+                        h264: [
+                            {
+                                bitrate: 180,
+                                file:
+                                    "https://pc.tedcdn.com/talk/stream/2019E/Blank/StefanAl_DeltaWorks_2019E-180k.mp4?dnt",
+                            },
+                        ],
+                        hls: {
+                            adUrl:
+                                "https://pubads.g.doubleclick.net/gampad/ads?ciu_szs=300x250%2C512x288%2C120x60%2C320x50%2C6x7%2C6x8&correlator=%5Bcorrelator%5D&cust_params=event%3DTED-Ed%26id%3D61316%26tag%3Deducation%2Cengineering%2Canimation%2CTED-Ed%2Cwater%2Ccities%2Cclimate%2Bchange%2Cdesign%2Cnature%2Coceans%2Cnatural%2Bdisaster%2Cinnovation%2Cinvention%2Ctechnology%2Cinfrastructure%26talk%3Dstefan_al_why_isn_t_the_netherlands_underwater%26year%3D2020&env=vp&gdfp_req=1&impl=s&iu=%2F5641%2Fmobile%2Fios%2Fweb&output=xml_vast2&sz=640x360&unviewed_position_start=1&url=%5Breferrer%5D",
+                            maiTargeting: {
+                                id: "61316",
+                                talk:
+                                    "stefan_al_why_isn_t_the_netherlands_underwater",
+                                tag:
+                                    "education,engineering,animation,TED-Ed,water,cities,climate change,design,nature,oceans,natural disaster,innovation,invention,technology,infrastructure",
+                                year: "2020",
+                                event: "TED-Ed",
+                            },
+                            stream: "https://hls.ted.com/talks/61316.m3u8",
+                            metadata: "https://hls.ted.com/talks/61316.json",
+                        },
+                    },
+                    targeting: {
+                        id: "61316",
+                        talk: "stefan_al_why_isn_t_the_netherlands_underwater",
+                        tag:
+                            "education,engineering,animation,TED-Ed,water,cities,climate change,design,nature,oceans,natural disaster,innovation,invention,technology,infrastructure",
+                        year: "2020",
+                        event: "TED-Ed",
+                    },
+                    canonical:
+                        "https://www.ted.com/talks/stefan_al_why_isn_t_the_netherlands_underwater",
+                    name: "Stefan Al: Why isn't the Netherlands underwater?",
+                    title: "Why isn't the Netherlands underwater?",
+                    speaker: "Stefan Al",
+                    thumb:
+                        "https://pi.tedcdn.com/r/talkstar-photos.s3.amazonaws.com/uploads/2ea833fe-d1ba-412b-9b77-a8ab1dfda151/deltaworkstextless.jpg?quality=89&w=600",
+                    slug: "stefan_al_why_isn_t_the_netherlands_underwater",
+                    event: "TED-Ed",
+                    published: 1585063759,
+                    external: {
+                        service: "YouTube",
+                        code: "25LW_PG2ZuI",
+                        duration: 324,
+                        start_time: 0,
+                    },
+                },
+            ],
+            recorded_at: "2020-03-24T00:00:00.000+00:00",
+            related_talks: [
+                {
+                    id: "32250",
+                    hero:
+                        "https://s3.amazonaws.com/talkstar-photos/uploads/2ba8ff9f-ee77-422e-97db-86c8b8d6279e/skyscraperstextless.jpg",
+                    speaker: "Stefan Al",
+                    title: "Will there ever be a mile-high skyscraper? ",
+                    duration: 284,
+                    slug: "stefan_al_will_there_ever_be_a_mile_high_skyscraper",
+                    viewed_count: 1477786,
+                },
+                {
+                    id: "58787",
+                    hero:
+                        "https://s3.amazonaws.com/talkstar-photos/uploads/c2a55a6d-5223-40e9-bf0c-521c001ba2e5/brooklynbridgetextless.jpg",
+                    speaker: "Alex Gendler",
+                    title: "Epic Engineering: Building the Brooklyn Bridge ",
+                    duration: 292,
+                    slug:
+                        "alex_gendler_epic_engineering_building_the_brooklyn_bridge",
+                    viewed_count: 316677,
+                },
+                {
+                    id: "36217",
+                    hero:
+                        "https://s3.amazonaws.com/talkstar-photos/uploads/be25233e-8e8c-4aff-aef4-cd3f9f47245d/glacier_textless.jpg",
+                    speaker: "M Jackson",
+                    title: "How to grow a glacier",
+                    duration: 299,
+                    slug: "m_jackson_how_to_grow_a_glacier",
+                    viewed_count: 310641,
+                },
+                {
+                    id: "31466",
+                    hero:
+                        "https://s3.amazonaws.com/talkstar-photos/uploads/541d915a-559f-43d0-be04-8f2f8d960c3c/KotchakornVoraakhom_2016W-embed.jpg",
+                    speaker: "Kotchakorn Voraakhom",
+                    title:
+                        "How to transform sinking cities into landscapes that fight floods",
+                    duration: 750,
+                    slug:
+                        "kotchakorn_voraakhom_how_to_transform_sinking_cities_into_landscapes_that_fight_floods",
+                    viewed_count: 1763806,
+                },
+                {
+                    id: "1583",
+                    hero:
+                        "https://s3.amazonaws.com/talkstar-photos/uploads/f6f4ba06-46ec-4bd7-b751-5a0e28c3540a/VickiArroyo_2012G-embed.jpg",
+                    speaker: "Vicki Arroyo",
+                    title: "Let's prepare for our new climate",
+                    duration: 876,
+                    slug: "vicki_arroyo_let_s_prepare_for_our_new_climate",
+                    viewed_count: 1172308,
+                },
+                {
+                    id: "2404",
+                    hero:
+                        "https://s3.amazonaws.com/talkstar-photos/uploads/770b4c44-0115-47b0-92f6-3c223024bcf2/DavidSedlak_2015X_embed.jpg",
+                    speaker: "David Sedlak",
+                    title: "4 ways we can avoid a catastrophic drought",
+                    duration: 817,
+                    slug:
+                        "david_sedlak_4_ways_we_can_avoid_a_catastrophic_drought",
+                    viewed_count: 1151750,
+                },
+            ],
+            slug: "stefan_al_why_isn_t_the_netherlands_underwater",
+            speakers: [
+                {
+                    id: "4061",
+                    slug: "stefan_al",
+                    is_published: false,
+                    firstname: "Stefan",
+                    lastname: "Al",
+                    middleinitial: "",
+                    title: "",
+                    description: "Architect",
+                    photo_url:
+                        "https://pe.tedcdn.com/images/ted/7f30930b0514fe977ef29739f17a76ad8da29d73_254x191.jpg",
+                    whatotherssay: "",
+                    whotheyare:
+                        "Architect and urban designer Stefan Al believes that architecture is more than just buildings.",
+                    whylisten: "",
+                },
+            ],
+            speaker_name: "Stefan Al",
+            tags: [
+                "education",
+                "engineering",
+                "animation",
+                "TED-Ed",
+                "water",
+                "cities",
+                "climate change",
+                "design",
+                "nature",
+                "oceans",
+                "natural disaster",
+                "innovation",
+                "invention",
+                "technology",
+                "infrastructure",
+            ],
+            title: "Why isn't the Netherlands underwater?",
+            video_type: {
+                id: "3",
+                name: "TED-Ed Original",
+            },
+            viewed_count: 0,
+        },
+    ],
+};
